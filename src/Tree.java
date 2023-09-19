db90d96880a0c7a6459364055a48f91904e5ea96
kaviraj <kavirajgaire@gmail.com>
2023-09-18 20:50:19 -0400

    fix everything

--- src/Tree.java
+++ src/Tree.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Tree<T> {
   // Private Attributes
   // The item stored at this tree's root, or null if the tree is empty.
-  private T root;
+  private Object root;
 
   // The list of all subtrees of this tree.
   private List<Tree<T>> subtrees;
@@ -19,7 +19,7 @@ public class Tree<T> {
     this.subtrees = new ArrayList<>(subtrees);
   }
 
-  public T getRoot() {
+  public Object getRoot() {
     return root;
   }
 
@@ -52,45 +52,44 @@ public class Tree<T> {
       }
       return num;
     }
+  }
 
   public double average(){
     if(this.is_empty()){
       return 0.0;
     }
-    count = this.Count();
-    size = this.length();
+    double count = this.average_helper();
+    int size = this.length();
     return count / size;
 
   }
-  private int average_helper(){
-    total = 0;
-    if(this.root){
-      total += 1;
+  private double average_helper(){
+    int total = 0;
+    if(this.root != null ){
+      total = total + this.root;
     }
-    for(Tree i: this.subtrees){
+    for(Tree<T> i: this.subtrees){
       total += this.average_helper();
     }
     return total;
   }
   @Override
-  public boolean equals(Object o) {
+  public boolean equals(Tree o) {
+
     if (this.is_empty() & o.is_empty()) {
       return true;
-    } else if (this.is_empty() || other.is_empty()) {
+    } else if (this.is_empty() || o.is_empty()) {
       return false;
     } else {
-      if (this.root != other._root) {
+      if (this.root != o.getRoot()) {
         return false;
       }
-      if (this.subtrees.length() != other._subtrees.length()) {
+      if (this.subtrees.size() != o.subtrees.size()) {
         return false;
       }
-      return self._subtrees == other._subtrees;
+      return this.subtrees == o.subtrees;
     }
   }
-}
-  }
-
   public int length() {
     if (this.is_empty()) {
       return 0;
@@ -103,3 +102,7 @@ public class Tree<T> {
     }
   }
 }
+
+
+
+
